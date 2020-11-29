668294364f81e700502a3da020ff3c426afb440d
Leah Pomerantz <leahp@ou.edu>
2020-11-29 15:31:09 -0500

    Initial File list

---
+++ R/testf.R
@@ -0,0 +1,19 @@
+#' Test Function
+#'
+#' Takes a vector and returns its square. Also gives a statement as to what the purpose is
+#'
+#' @param x
+#'
+#' @return a vector of squared values
+#' @return a string of characters
+#'
+#' @export
+#'
+#' @examples
+#' x=1:30; testf(x)
+testf <- function(x){
+  x.sq <- x^2
+  char <- "This is a test to see that I have correctly set up my package and built a basic workable function"
+  test.list <- list("x squared" = x.sq, char)
+  return(test.list)
+}
